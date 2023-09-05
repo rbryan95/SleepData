1cd8bc915abb99a2335353c600eebb9b0980a8fe
rbryan95 <berd1495@gmail.com>
2023-09-05 14:22:52 -0500

    Week of Data input

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// ask for input
+Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data.");
+Console.WriteLine("Enter anything else to quit.");
+// input response
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // TODO: create data file
+}
+else if (resp == "2")
+{
+    // TODO: parse data file
+    // create data file
+
+    // ask a question
+    Console.WriteLine("How many weeks of data is needed?");
+    // input the response (convert to int)
+    int weeks = int.Parse(Console.ReadLine());
+        // determine start and end date
+    DateTime today = DateTime.Now;
+    // we want full weeks sunday - saturday
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+    // subtract # of weeks from endDate to get startDate
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+    Console.WriteLine(dataDate);
+}
+    
